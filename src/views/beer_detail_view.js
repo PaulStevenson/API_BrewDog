bfad4723a1ea5d044f312498efa28137f6a5fc25
Paul <rpmstevenson@gmail.com>
2018-06-29 14:50:27 +0100

    tagline and description displayed

--- src/views/beer_detail_view.js
+++ src/views/beer_detail_view.js
@@ -9,13 +9,15 @@ BeerDetailView.prototype.createBeerDetail = function (beer) {
     const beerName = document.createElement('h2');
     beerName.textContent = beer.name;
     beerDetail.appendChild(beerName);
-    return beerDetail;
 
 
     const detailsList = document.createElement('ul');
-    console.log(detailList);
     const tagline = this.createDetailListItem('Tagline', beer.tagline);
-    detailList.appendChild(tagline);
+    detailsList.appendChild(tagline);
+
+    const description = this.createDetailListItem('Description', beer.description);
+    detailsList.appendChild(description);
+
 
     beerDetail.appendChild(detailsList);
     return beerDetail;
@@ -24,10 +26,7 @@ BeerDetailView.prototype.createBeerDetail = function (beer) {
 BeerDetailView.prototype.createDetailListItem = function (label, property) {
     const element = document.createElement('li');
     element.textContent = `${label}: ${property}`;
-    console.log(element);
     return element;
-
-
 };
 
 module.exports = BeerDetailView;
